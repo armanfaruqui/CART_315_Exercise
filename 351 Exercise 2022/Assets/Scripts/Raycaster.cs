f794a8d60c0f1171687638ece7e59b72185b0922
armanfaruqui <70916901+armanfaruqui@users.noreply.github.com>
2022-02-04 11:32:25 -0500

    Remembered to push exercise

--- 351 Exercise 2022/Assets/Scripts/Raycaster.cs
+++ 351 Exercise 2022/Assets/Scripts/Raycaster.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotate : MonoBehaviour
+public class Raycaster : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,7 +13,6 @@ public class Rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-            transform.Rotate (0,50*Time.deltaTime,0); //rotates 50 degrees per second around z axis
-
+        
     }
 }
